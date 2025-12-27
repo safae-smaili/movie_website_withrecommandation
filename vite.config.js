f3e40030656244d453bbc470a116abcbc15fb60f
safae-smaili <safaesmaili.s@gmail.com>
2025-12-27 20:56:11 +0100

    removing github-pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   // base:"/movies/",
+//   base: process.env.VITE_BASE_PATH || "/movies/",
+
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base:"/movies/",
-  base: process.env.VITE_BASE_PATH || "/movies/",
+  base: '/',  // Important pour Vercel
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
 })
