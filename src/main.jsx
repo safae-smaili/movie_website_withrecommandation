5d12cfcc206a2b87eeb31058bab91a6a6a4cb0ec
safae-smaili <safaesmaili.s@gmail.com>
2025-12-27 20:59:24 +0100

    corection

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,8 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/movies/">
+    {/* <BrowserRouter basename="/movies/"> */}
+  <BrowserRouter>
     <App />
    </BrowserRouter>
   </StrictMode>,
